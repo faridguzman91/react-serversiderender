c23b76cc5eb6a7251b0e5617176b69a652a1a3d9
Jimmy Guzman <jimmy@creapolis.com>
2021-09-30 13:19:30 +0200

    serviceworker en card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,3 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
-import Owner from '../Owner/Owner';
+import Owner from '../owner/owner';
+
+const CardWrapper = styled.div`
+text-align: left;
+padding: 1%;
+background: lightGray;
+border-radius: 5px;
+margin-bottom: 2%;
+`;
+
+const Title = styled.h2`
+width: 100%;
+padding-bottom: 10px;
+text-align: center;
+border-bottom: 1px solid darkGray;
+`;
+
+const Meta = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+margin: 0 2%;
+`;
+
+const Count = styled.div`
+flex-basis: 80%;
+`
+
+const Card = ({ data }) => (
+    <CardWrapper>
+        <Title>{data.title}</Title>
+        <Meta>
+        <Count>
+            {`Bekeken: ${data.view_count} | Antwoorden: ${data.answer_count}`}
+        </Count>
+        </Meta>
+    </CardWrapper>
+)
+
+export default Card;
